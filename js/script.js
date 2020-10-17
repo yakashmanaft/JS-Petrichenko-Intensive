5f169b4d88714957adb48afce0200c597b0ab0af
Sergey <anfalov@camini-pr.ru>
2020-10-17 11:18:29 +0500

    Третий урок

--- js/script.js
+++ js/script.js
@@ -1,19 +1,62 @@
+window.addEventListener('DOMContentLoaded', function () {
+    let products = document.querySelectorAll('.product'),
+        buttons = document.querySelectorAll('button'),
+        openBtn = document.querySelector('.open');
 
+    function createCart() {
+        let cart = document.createElement('div'),
+        field = document.createElement('div'),
+        heading = document.createElement('h2'),
+        closeBtn = document.createElement('button');
 
+        cart.classList.add('cart');
+        field.classList.add('cart-field');
+        closeBtn.classList.add('close');
 
+        heading.textContent = 'В нашей корзине:';
+        closeBtn.textContent = 'Закрыть';
 
-document.addEventListener('DOMContentLoaded', function () {
+        document.body.appendChild(cart);
+        cart.appendChild(heading);
+        cart.appendChild(field);
+        cart.appendChild(closeBtn);
+    }
 
-    let chooseBtn = document.getElementById('#choose'),
-    receiveBtn = document.getElementById('#receive'),
-    heading = document.getElementsByTagName('h2')[0],
-    nameInput = document.getElementsByClassName('contactform_name')[0],
-    phoneInput = document.querySelector('.contactform_phone'),
-    mailInput = document.querySelectorAll('.contactform_mail')[0],
-    modal = document.querySelector('.modal');
+    createCart();
 
+    let field = document.querySelector('.cart-field'),
+        cart = document.querySelector('.cart'),
+        close = document.querySelector('.close');
+    
+    function openCart() {
+        cart.style.display = 'block';
+    }
+    function closeCart() {
+        cart.style.display = 'none';
+    }
 
-    receiveBtn.addEventListener('click', () => {
-        modal.style.display = 'block';
+    openBtn.addEventListener('click', openCart);
+    close.addEventListener('click', closeCart);
+
+    // for (let i = 0; i < buttons.length; i++) {
+    //     buttons[i].addEventListener('click', function () {
+    //         let item = products[i].cloneNode(true),
+    //             btn = item.querySelector('button');
+
+    //         btn.remove(); 
+    //         field.appendChild(item);
+    //         products[i].remove();
+    //     });
+    // }
+
+    buttons.forEach((item, i) => {
+        item.addEventListener('click', () =>{
+            let item = products[i].cloneNode(true),
+                btn = item.querySelector('button');
+
+            btn.remove(); 
+            field.appendChild(item);
+            products[i].remove();
+        });
     });
 });
